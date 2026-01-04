9599a78ecd526d2efe375865b9aba52a65f8a2f0
Abdullah Zafar <abdullah.zafar.career@gmail.com>
2026-01-04 22:01:12 +0500

    removed special

--- apps/mobile/src/components/special/stories/index.ts
+++
@@ -1,8 +0,0 @@
-export {default as LoveStoryModal} from "./LoveStoryModal";
-export {default as HeartIntroScreen} from "./HeartIntroScreen";
-export {default as RoseBloomScreen} from "./RoseBloomScreen";
-export {default as DrawingRose} from "./DrawingRose";
-export {default as RainbowScreen} from "./RainbowScreen";
-export {default as SunshineScreen} from "./SunshineScreen";
-export {default as CloseConfirmModal} from "./CloseConfirmModal";
-
