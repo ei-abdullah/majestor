aeb1e6fd17ee7c0889a3100ace5fff8a1e1a18fb
Abdullah Zafar <abdullah.zafar.career@gmail.com>
2026-01-11 17:56:01 +0500

    update: add files to .gitignore

--- apps/mobile/src/components/screens/carpool/CarpoolHome.tsx
+++ apps/mobile/src/components/screens/carpool/CarpoolHome.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Platform, Text} from "react-native";
 import {GoogleMaps, AppleMaps} from "expo-maps";
 
+import * as Location from 'expo-location';
+
 export default function CarpoolHome() {
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function getCurrentLocation() {
+
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if(status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        }
+
+        getCurrentLocation();
+    }, []);
 
     if (Platform.OS === 'ios') {
         return <AppleMaps.View style={{flex: 1}}/>;
@@ -11,8 +31,8 @@ export default function CarpoolHome() {
             style={{flex: 1}}
             cameraPosition={{
                 coordinates: {
-                    latitude: 37.78825,
-                    longitude: -122.4324,
+                    latitude: location?.coords.latitude ?? 37.78825,
+                    longitude: location?.coords.longitude ?? -122.4324,
                 },
                 zoom: 15,
             }}
