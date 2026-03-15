3828545d0a671e9ceb796aef3bb9449b628979dd
Abdullah Zafar <abdullah.zafar.career@gmail.com>
2026-03-15 15:10:25 +0500

    Integrated Sentry for error logging

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,11 +1,18 @@
+import { TextEncoder, TextDecoder } from "text-encoding"
+import { Buffer } from 'buffer';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+global.Buffer = Buffer;
+
 import React, {useEffect, useState} from "react";
 import {Stack} from "expo-router";
 import Toast from "react-native-toast-message";
 import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
 import {ActivityIndicator, View} from "react-native";
+import * as Sentry from "@sentry/react-native"
 
 import "./global.css"
-
 import {useAuthStore} from "@/src/stores/authStore";
 
 const client = new QueryClient({
@@ -17,11 +24,26 @@ const client = new QueryClient({
     }
 });
 
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-global.Buffer = Buffer;
+Sentry.init({
+    dsn: 'https://955f5849bf09988aeed526b5b587d901@o4511044632903680.ingest.de.sentry.io/4511044954423376',
 
-export default function RootLayout() {
+    // Adds more context data to events (IP address, cookies, user, etc.)
+    // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+    sendDefaultPii: true,
+
+    // Enable Logs
+    enableLogs: true,
+
+    // Configure Session Replay
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: __DEV__,
+});
+
+function RootLayout() {
     const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
     const [hydrated, setHydrated] = useState(useAuthStore.persist.hasHydrated());
 
@@ -58,3 +80,5 @@ export default function RootLayout() {
         </React.Fragment>
     )
 }
+
+export default Sentry.wrap(RootLayout);
